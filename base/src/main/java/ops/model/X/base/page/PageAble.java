87fbca88f8846db1b9f2656ad9d73d9beaef4e03
tong'zhu'he <tongzhuhe@lidig.com>
2019-08-15 17:16:52 +0800

    tzh

---
+++ base/src/main/java/ops/model/X/base/page/PageAble.java
@@ -0,0 +1,39 @@
+package ops.model.X.base.page;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+
+public class PageAble {
+
+    private Integer page;
+
+    private Integer size;
+
+
+    public PageAble(Integer page, Integer size) {
+        this.page = page;
+        this.size = size;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public Page getPage() {
+        if (this.page == null) {
+            this.page = 1;
+        }
+        if (this.size == null) {
+            this.size = 10;
+        }
+        return new Page(this.page, this.size);
+    }
+
+}
