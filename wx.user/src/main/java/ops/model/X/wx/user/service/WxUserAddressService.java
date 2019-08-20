336feca4cfc8135815a97483d0238a0f0f58e488
tong'zhu'he <tongzhuhe@lidig.com>
2019-08-20 10:46:54 +0800

    tzh

--- wx.user/src/main/java/ops/model/X/wx/user/service/WxUserAddressService.java
+++ wx.user/src/main/java/ops/model/X/wx/user/service/WxUserAddressService.java
@@ -4,4 +4,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import ops.model.X.wx.user.entity.WxUserAddress;
 
 public interface WxUserAddressService extends IService<WxUserAddress> {
+
+
 }
