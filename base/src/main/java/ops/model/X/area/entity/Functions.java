cb9c1d8584156746eab499cc8b10ee8c790d01a9
tong'zhu'he <tongzhuhe@lidig.com>
2019-09-09 15:45:44 +0800

    tzh

--- base/src/main/java/ops/model/X/area/entity/Functions.java
+++ base/src/main/java/ops/model/X/area/entity/Functions.java
@@ -175,7 +175,9 @@ public class Functions implements BaseEntity {
     }
 
     public void beAdd() {
-
+        if (this.sort == null) {
+            this.sort = System.currentTimeMillis();
+        }
     }
 
     public void beUpdate() {
